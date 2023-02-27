08afbd4c0e7b1e251b9e8143d4628029b983101f
nathanhettige <67953260+nathanhettige@users.noreply.github.com>
2023-02-27 20:13:07 +1000

    useRef for codeChellange

--- src/api/auth.tsx
+++ src/api/auth.tsx
@@ -1,17 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 /**
  * PKCE for OAuth2.0
  */
 
 export const useSpotifyLogin = () => {
-  const [codeChallenge, setCodeChallenge] = useState<string | null>(null);
+  const codeChallenge = useRef<string | null>(null);
   const [loading, setLoading] = useState(true);
   const verifier = window.crypto.randomUUID(); // Compatability issues with older browsers
 
   useEffect(() => {
     generateCodeChallenge(verifier)
-      .then(setCodeChallenge)
+      .then((challenge) => {
+        codeChallenge.current = challenge;
+      })
       .catch(console.error)
       .finally(() => {
         setLoading(false);
@@ -23,13 +25,13 @@ export const useSpotifyLogin = () => {
     response_type: 'code',
     client_id: import.meta.env.VITE_SPOTIFY_CLIENT_ID as string,
     code_challenge_method: 'S256',
-    code_challenge: codeChallenge as string,
+    code_challenge: codeChallenge.current as string,
     redirect_uri: import.meta.env.VITE_REDIRECT_URI as string
   });
 
   const redirectToLogin = () => {
     if (codeChallenge == null) {
-      throw new Error('Check loading state before calling redirect f');
+      throw new Error('Check loading state before calling redirect');
     } else {
       window.location.href = `https://accounts.spotify.com/authorize?${authQueryParams.toString()}`;
     }
